1a1eda0026fe5424303105f9341e381ccd2d565e
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2022-07-18 21:30:18 +0530

    Email view alternated, Search query fixed, flash message after signUp

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,16 +6,16 @@ class SearchController < ApplicationController
 
     def show
         data = []
-        query = params[:id].downcase
+        query = params[:id]
         case params[:type]
         when "Name"
-            users = User.where('name like ?', "%"+ User.sanitize_sql_like(query) +"%")
+            users = User.where("name "+ ENV["like"] +" ?", "%"+ User.sanitize_sql_like(query) +"%")
             data = users.map {|i| {"id": i.id, "name": i.name, "url": i.image_url, "tags": [] }}
         when "Language"
             users = User.all.to_a.select{ |i| i.languages.length > 0 }
             data = []
             users.each do |i|
-                languages = i.languages.where('name like ?', "%"+ Language.sanitize_sql_like(query) +"%" ).to_a
+                languages = i.languages.where("name "+ ENV["like"] +" ?", "%"+ Language.sanitize_sql_like(query) +"%" ).to_a
                 if languages != []
                     data << {"id": i.id, "name": i.name, "url": i.image_url, "tags": languages }
                 end
@@ -24,7 +24,7 @@ class SearchController < ApplicationController
             users = User.all.to_a.select{ |i| i.skills.length > 0 }
             data = []
             users.each do |i|
-                skills = i.skills.where('name like ?', "%"+ Skill.sanitize_sql_like(query) +"%" ).to_a
+                skills = i.skills.where("name " + ENV["like"] + " ?", "%"+ Skill.sanitize_sql_like(query) +"%" ).to_a
                 if skills != []
                     data << {"id": i.id, "name": i.name, "url": i.image_url, "tags": skills }
                 end
