d933cd783bea63c1f699a6389c2308426933252a
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2022-07-19 09:09:43 +0530

    Email view alternated, Search query fixed, flash message after signUp

--- app/helpers/skills_helper.rb
+++ app/helpers/skills_helper.rb
@@ -4,6 +4,7 @@ module SkillsHelper
         base_uri 'api.stackexchange.com'
 
         def find name
+            name.downcase!
             options = { query: { order: "desc", sort: "popular", site: "stackoverflow", inname: name}}
             self.class.get("/2.3/tags", options)["items"].map{ |i| i["name"] }.select{ |i| (i =~ /\d/) == nil }
         end
