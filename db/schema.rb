ef28faba8927daa2fca120c4b2872bb7224ed98a
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2023-02-18 14:43:12 +0530

    Admin field in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_065329) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_090603) do
   create_table "achievements", force: :cascade do |t|
     t.string "description"
     t.string "image_url"
@@ -203,6 +203,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_065329) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.boolean "admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
