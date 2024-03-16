9a6893712777746bee0d88866ba683c49d7508c0
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2024-03-16 12:26:41 +0530

    asset pipeline fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( application.scss )
+Rails.application.config.assets.precompile += %w( application.scss active_admin.scss )
